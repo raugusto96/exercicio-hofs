0c0e7cc0bae0b6606dd7357a30747a7a7fde986a
Rodrigo <rodrigoaugusto96@outlook.com>
2021-11-23 18:51:55 -0300

    Add exercise 5

---
+++ HOFs/src/getTypeByName.js
@@ -0,0 +1,21 @@
+/*
+  Complete a função getTypeByName que deve receber um parametro sendo o nome do pokemon
+  e deve retornar o array de tipos do pokemon.
+
+  Dica: A função deve ser case insensitive.
+
+  Parametro:
+  name => string
+
+  Comportamento:
+  getTypeByName() // Retorno: undefined;
+  getTypeByName('Bulbasaur') // Retorno: ['grass', 'poison'];
+  getTypeByName('xablau') // undefined;
+
+*/
+
+const { pokemons } = require('../data');
+
+const getTypeByName = () => {};
+
+module.exports = getTypeByName;
